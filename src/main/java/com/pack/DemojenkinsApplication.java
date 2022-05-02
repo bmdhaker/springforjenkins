91d828af2973cafa48b4889c604202ab6c0f7596
bmdhaker <bmdhaker@gmail.com>
2022-05-02 06:26:09 +0100

    Adding CommandLineRunner to test and display employees

--- src/main/java/com/pack/DemojenkinsApplication.java
+++ src/main/java/com/pack/DemojenkinsApplication.java
@@ -1,7 +1,12 @@
 package com.pack;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.pack.model.Employee;
+import com.pack.repository.EmployeeRepository;
 
 @SpringBootApplication
 public class DemojenkinsApplication {
@@ -9,5 +14,21 @@ public class DemojenkinsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemojenkinsApplication.class, args);
 	}
+	@Bean
+    public CommandLineRunner demo(EmployeeRepository repository) {
+        return (args) -> {
+
+            // Create users with BCrypt encoded password (user/user, admin/admin)
+            Employee mnarios =new Employee("Mnari","Dhaker","bm@gm.com");
+            Employee marroula =new Employee("Mnari","Miral","bmm@gm.com");
+            Employee mouhamed =new Employee("Mnari","Mouhammed","b2m@gm.com");
+            repository.save(mnarios);
+            repository.save(marroula);
+            repository.save(mouhamed);
+            repository.findAll().forEach(e->{
+            	System.out.println(e.toString());
+            });
+        };
+    }
 
 }
